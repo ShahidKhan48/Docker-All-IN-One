6c2cadf272f2d40e859d6f256c73f1bd801d7be0
shahidkhan48 <awskhan777@gmail.com>
2025-11-27 16:51:02 +0530

    updated

--- application/static-app/Terraform/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-1" # Or your desired AWS region
-}
-
-# Terraform will automatically look for AWS credentials in
-# environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-# or an IAM role if running on EC2.
--- application/static-app/shell-script/myarrya.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is an associative array (key-value pair)
-
-declare -A myarray
-
-myarray=(["name"]="shahid" ["age"]="29"  ["city"]="bangalore")
-
-echo "My name is ${myarray["name"]}" 
